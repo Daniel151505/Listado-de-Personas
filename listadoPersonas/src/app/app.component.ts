e64e0d9cd682ab616d67f2d39598ee973037204a
Daniel151505 <daniel150520011@gmail.com>
2021-01-16 10:19:34 -0500

    Mandando Alerta

--- listadoPersonas/src/app/app.component.ts
+++ listadoPersonas/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoggingService } from './LoggingService.service';
+import { Persona } from './persona.model';
+import { PersonasService } from './persona.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'listadoPersonas';
+export class AppComponent implements OnInit {
+  //variable
+  titulo = 'Listado de Personas';
+
+  //arreglo
+  personas: Persona[] = [];
+
+  //llamando servicios y mandando alerta
+  constructor(private loggingService:LoggingService,
+              private personasService: PersonasService){
+                this.personasService.saludar.subscribe(
+                  (indice: number) => alert("El índice es: " + (indice + 1) )
+                );
+              }
+// igualando arreglo con el arreglo del servicio
+  ngOnInit(): void {
+    this.personas = this.personasService.personas;
+  }
+
 }
