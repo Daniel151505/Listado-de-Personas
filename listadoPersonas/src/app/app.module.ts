16f08ebf042f37a8d66d9d22b85323659f818451
Daniel151505 <daniel150520011@gmail.com>
2021-01-15 20:08:27 -0500

    Creando Componente Persona

--- listadoPersonas/src/app/app.module.ts
+++ listadoPersonas/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { PersonaComponent } from './persona/persona.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioComponent
+    FormularioComponent,
+    PersonaComponent
   ],
   imports: [
     BrowserModule,
